77f980c0209a13144776a0f95591006fc6a498f0
Konstantin Babintsev <kbabintsev@gmail.com>
2019-04-26 14:24:30 +0500

    1.0.4 x-google-allow injected

--- src/main/java/com/cloudaware/EndpointsMojo.java
+++ src/main/java/com/cloudaware/EndpointsMojo.java
@@ -2,10 +2,14 @@ package com.cloudaware;
 
 import com.google.api.server.spi.tools.EndpointsTool;
 import edu.emory.mathcs.backport.java.util.Collections;
+import org.apache.commons.io.IOUtils;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -57,17 +61,24 @@ public final class EndpointsMojo extends AbstractMojo {
             args.addAll(apis);
             args.add("com.google.api.server.spi.discovery.ProxyingDiscoveryService");
             endpointsTool.execute(args.toArray(new String[args.size()]));
+
+            // https://cloud.google.com/endpoints/docs/openapi/openapi-extensions#x-google-allow
+            final String content = IOUtils.toString(new FileInputStream(outputSwaggerDoc), StandardCharsets.UTF_8.name());
+            IOUtils.write(content.replaceFirst("\\{", "{\n \"x-google-allow\": \"all\","), new FileOutputStream(outputSwaggerDoc), StandardCharsets.UTF_8.name());
+
             if (!outputDiscoveryDocs.exists()) {
                 outputDiscoveryDocs.mkdirs();
             } else if (!outputDiscoveryDocs.isDirectory()) {
                 throw new Exception("Discovery is not folder");
             }
+
             for (final String path : outputDiscoveryDocs.list()) {
                 final File file = new File(outputDiscoveryDocs, path);
                 if (file.isFile()) {
                     file.delete();
                 }
             }
+
             args = new ArrayList<String>();
             args.add("get-discovery-doc");
             args.add("--hostname=" + hostname);
@@ -75,7 +86,6 @@ public final class EndpointsMojo extends AbstractMojo {
             args.add("--output=" + outputDiscoveryDocs);
             args.addAll(apis);
             endpointsTool.execute(args.toArray(new String[args.size()]));
-
         } catch (Exception e) {
             throw new MojoExecutionException(e.getMessage(), e);
         }
